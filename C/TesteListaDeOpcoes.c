1e727f239317cfecbf3139262ff01f9273661258
Lucas Andrada <laaandrd@protonmail.com>
2021-03-03 16:25:26 -0300

    Implementação da lista de opções comentada

--- C/TesteListaDeOpcoes.c
+++ C/TesteListaDeOpcoes.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Estrutura da lista de opções: facilita o acesso aos textos das opções
+//Tomei como base 5 opções por decisão a ser tomada
+//O ponteio indica qual é a posição selecionada (>>)
 struct listaDeOpcoes {
 
     char titulo [280];
@@ -9,7 +12,7 @@ struct listaDeOpcoes {
     char opcoes [5][20];
 };
 
-
+//Acessa os atributos da estrutura para imprimir o poneiro junto a opção
 void impListOpc(struct listaDeOpcoes *lista){
 
   printf("%s\n", lista->titulo);
@@ -22,21 +25,24 @@ void impListOpc(struct listaDeOpcoes *lista){
   }
 }
 
+//Possibilida o movimento do ponteiro, trocando o valor (>>) com o possível valor nulo (  ) de seus vizinhos
 void mudarOpcao(struct listaDeOpcoes *lista){
+
+//"aux" indica a posição em que o ponteiro está
   int aux = 0;
+  //Percorre as opções buscando onde está o ponteiro
   for(int i = 0; i<5; i++){
     if(strcmp(lista->ponteiro[i], ">>") == 0){
       aux = i;
       i = 5;
     }
-  }
   int codigoTecla = 0;
   //d = 100 ASCII
+  //O loop espera as entradas para movimentar o ponteiro (w/s) e fazer a escolha (d)
   while(codigoTecla != 100){
-
     codigoTecla = getchar();
-
     //w = 119 ASCII
+    //Verifica se o ponteiro pode ser movido pra cima
     if(codigoTecla == 119){
       for(int i = aux -1; i >= 0; i--){
         if( (strcmp(lista->ponteiro[i], "  ") == 0) && (strcmp(lista->opcoes[i], "NULL") != 0) ){
@@ -48,8 +54,8 @@ void mudarOpcao(struct listaDeOpcoes *lista){
         }
       }
     }
-
     //s = 115 ASCII
+    //Verifica se o ponteiro pode ser movido pra baixo
     if(codigoTecla == 115){
       for(int i = aux + 1; i <5; i++){
         if( (strcmp(lista->ponteiro[i], "  ") == 0) && (strcmp(lista->opcoes[i], "NULL") != 0) ){
@@ -62,9 +68,8 @@ void mudarOpcao(struct listaDeOpcoes *lista){
       }
     }
   }
-
+  //Imprime a lista atualizada a cada modificação
   impListOpc(lista);
-
 }
 
 int main() {
@@ -77,9 +82,12 @@ int main() {
   strcpy(lista.opcoes[1], "Esconder");
   strcpy(lista.opcoes[2], "Lutar");
   strcpy(lista.opcoes[3], "Aliar");
+  //Utilizando a plavara "NULL" para indicar a ausência da opção
   strcpy(lista.opcoes[4], "NULL");
 
+  //Utilizando ">>" para indicar o ponteiro
   strcpy(lista.ponteiro[0], ">>");
+  //Utilizando "  " para indicar a ausência do ponteiro
   strcpy(lista.ponteiro[1], "  ");
   strcpy(lista.ponteiro[2], "  ");
   strcpy(lista.ponteiro[3], "  ");
