1db72cfb505ff445ce3ebf4acf0ab48958fff5c0
Yves-Medhard Tibe da Cunha Tibe-Bi <yvesmedhard+github@gmail.com>
2021-03-07 21:36:06 -0300

    ft: Proposta de estrutura de cenas evolutiva.

---
+++ C/jogo.h
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include "cenas.h"
+#include "helpers.h"
+
+int proximaCena(Cena cena);
+void exibirCena(Cena cena);
+void loopDeCenas(int numDaCena);
+
+void novoJogo() {
+  printf("Novo Jogo\n");
+  carregarCenas();
+
+  loopDeCenas(0);
+  /* defini que a cena 0 é a inicial por simplicidade
+  Se for o caso podemos transformar em uma Constante e dependendo do jogo começa em um indice diferente.
+  */
+}
+
+/* Esse loopDeCenas atua como um função recursiva que explora o uso da variável global com todas as cenas disponíveis
+e recursão de cauda ao definir qual é a próxima cena.
+As principais mecânicas de jogo vão ser chamadas aqui através de outras funções que podem ser especializadas.
+*/
+void loopDeCenas(int numDaCena) {
+  //Arrays não podem ter indices negativos isso previne qualquer input errado de executar essa função
+  if(numDaCena < 0){
+    return;
+  } else {
+    Cena cenaAtiva = CENAS[numDaCena];
+    exibirCena(cenaAtiva);
+
+    if(cenaAtiva.fimDeJogo) {
+      return;
+    } else {
+      int escolha = proximaCena(cenaAtiva);
+      // O retorno é nessa função para utilizar recursão de cauda.
+      return loopDeCenas(escolha);
+    }
+  }
+}
+
+/* Podemos colocar aqui a forma de exibir a cena.
+Podemos evoluir ela para ter outras propriedades a serem exploradas nessa parte do programa */
+void exibirCena(Cena cena) {
+  printf("\n\n");
+  printf("%s", cena.descricao);
+}
+
+// Aqui podemos evoluir a cena para ter a lista de próximas cenas disponíveis e nessa função validar isso. Podemos trazder mais tipos de iteração também (como por exemplo se a cena permitir fazer um combate antes de trocar de cena).
+int proximaCena(Cena cena) {
+  printf("Sua escolha: ");
+  return readInt();
+}
--- C/main.c
+++ C/main.c
@@ -3,75 +3,91 @@
 #include <string.h>
 #include "textos.h"
 #include "andares.h"
+#include "jogo.h"
+#include "helpers.h"
 
-int main(){
-  int resposta = 1, tamanho;
-  while (resposta != 0){ /*Enquanto o usuário quiser jogar.*/
-    printf("\nATENCAO: DIGITE O NUMERO QUE CORRESPONDE A OPCAO.\n(1) JOGAR.\n(2) SAIR.\nRESPOSTA: ");
-    scanf("%d",&resposta);
-    printf("\n");
-    switch(resposta){
-      case 1: /*Se o usuário quiser jogar.*/
-        for (int c=1; c<=3; c++){ /*Loop para percorrer os 3 andares com armadilhas.*/
-          int resp = 1;
-          int escolha = 1;
-          while (escolha!=0){ /*Enquanto a resposta for inválida.*/
-            andar_texto(c); /*Texto do andar c.*/
-            scanf("%d", &escolha);
-            switch (escolha) { /*Escolha do caminho do andar.*/
-              case 1:
-                andar(c,1);
-                escolha = 0; /*Para o loop, vai para o próximo andar.*/
-                break;
-              case 2:
-                andar(c,2);
-                escolha = 0; /*Para o loop, vai para o próximo andar.*/
-                break;
-              case 3: /*Opção continuar*/
-                escolha = 0; /*Para o loop, vai para o próximo andar.*/
-                break;
-              default: /*Caso a entrada seja inválida.*/
-                printf("\nOPCAO INVALIDA, TENTE NOVAMENTE.\n");
-                escolha = 1; /*Retorna ao loop*/
-                break;
-            }
-          }
-          while (resp != 0){ /*Pergunta se deseja continuar ou sair a cada andar.*/
-            printf("\nDESEJA CONTINUAR?\n(1) SIM.\n(2) SAIR.\nRESPOSTA: ");
-            scanf("%d", &resp);
-            switch (resp){
-              case 1: /*Apenas continua.*/
-                printf("\nSUBINDO DE ANDAR, AGUARDE...\n");
-                resp = 0;
-                break;
-              case 2: /*Encerra o programa.*/
-                printf("\nNOS VEMOS EM BREVE. ATE MAIS!\n");
-                exit(0);
-              default: /*Entrada inválida.*/
-                printf("\nOPCAO INVALIDA, TENTE NOVAMENTE.\n");
-                resp = 1;
-                break;
-            }
-          }
-        }
-      /*Fim do jogo*/
-      printf("\nPARABENS, VOCE ZEROU!");
-      /*printf("\nITENS GANHOS:");
-      printf("ITENS:");
-        for (int c=0; c<=3; c++){
-          printf("\n%s", *inventario[c]);
-        }*/
-      printf("\nNOS VEREMOS NOVAMENTE, ATE MAIS!");
-      resposta = 0;
-      break;
-    case 2: /*Se o jogador não quiser jogar.*/
-      printf("NOS VEMOS EM BREVE. ATE MAIS!");
-      resposta = 0; /*Para o loop*/
-      break;
-    default: /*Se a entrada for inválida*/
-      printf("OPCAO INVALIDA, TENTE NOVAMENTE.\n");
-      resposta = 1; /*Retornar ao loop*/
-      break;
-    }
+/* Loop de Jogo:
+1 - Menu com opções Jogar e Sair.
+Caso Sair encerra programa.
+Caso Jogar inicia um novo jogo e entra no loop a partir da primeira cena.
+Cenas podem ser de vitória, derrota ou levar a outra cena.
+Cada cena tem uma descrição, pode pedir para o jogador fazer alguma ação que pode levar a outra cena.
+O jogador interage via inputs no teclado.
+Caso a cena seja vitória ou derrota o jogador volta ao Menu.
+*/
+
+int escolhaValida(int escolha);
+int menuDeEscolha();
+void agradecimentos();
+void creditos();
+void menuLoop();
+
+int main()
+{
+  creditos();
+  menuLoop();
+  agradecimentos();
+}
+
+void creditos()
+{
+  printf("Esse jogo é parte das avaliações da matéria de PE da UFABC.. feito por....");
+  breakLines(2);
+}
+
+void agradecimentos()
+{
+  printf("Obrigado por jogar Nome do Jogo");
+  breakLines(1);
+}
+
+// Essas duas funções são relacionadas ao menu de escolhas do jogo.
+int menuDeEscolha()
+{
+  breakLines(1);
+  printf("----------------------------------------");
+  breakLines(2);
+  printf("DIGITE J PARA JOGAR ou S PARA SAIR: ");
+  int escolha = readChar();
+  switch (escolha) {
+  case 'S':
+    return 0;
+    break;
+  case 'J':
+    return 1;
+    break;
+  default:
+    return menuDeEscolha();
+    break;
   }
+}
+
+int escolhaValida(int escolha)
+{
+  return escolha == 1 || escolha == 0;
+}
+
+void menuLoop()
+{
+  int sair = 0;
+  do
+  {
+    int opcaoEscolhida = menuDeEscolha();
+    if (escolhaValida(opcaoEscolhida))
+    {
+      switch (opcaoEscolhida)
+      {
+      case 1:
+        novoJogo();
+        break;
+      case 0:
+        sair = 1;
+        break;
+      default:
+        break;
+      }
+    } else {
+      opcaoEscolhida = 3;
+    }
+  } while (sair != 1);
 }
